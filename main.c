e1447d6ef0ee5e523e1585490b6b6d9c87e2912a
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-27 22:16:11 +0800

    feat: implemented character conversion of capital and small letters

--- main.c
+++ main.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 
+char convert_char(char c) {
+    if (c >= 0xffffff80 && c <= 0xffffff99) {
+        return c - 0xffffff80 + 65;
+    }
+
+    if (c >= 0xffffffa0 && c <= 0xffffffb9) {
+        return c - 0xffffffa0 + 97;
+    }
+
+    return 0;
+}
+
+char* convert_text(char* string, int length) { 
+    for (int i = 0; i < length; i++) {
+        string[i] = convert_char(string[i]);
+    }
+
+    return string;
+}
+
 int main() {
     FILE *fp;
-    char name[10];
+    char name[11];
 
-    fp = fopen("debug\\target.sav", "r");
+    fp = fopen("debug\\target2.sav", "r");
 
     if (fp != NULL) {
         printf("Save File Recognized \n");
-        printf("Position: %d \n", ftell(fp));
         fseek(fp, 0x2598, SEEK_SET);
-        printf("Position: %d \n", ftell(fp));
         fread(name, sizeof(name), 1, fp);
-        printf("Name: %s", name);
+
+        for (int i = 0; i < 11; i++)
+            printf("%04x ", name[i]);
+
+        printf("\n");
+
+        printf("Name: %s", convert_text(name, 11));
+        
     }
     else {
         printf("ERROR: FileNotFoundException");
