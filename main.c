1b2e3319cfdfb94c4eee561be62b6860e6db6e42
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-30 20:06:53 +0800

    refactor: moved convert_char() and convert_text() into char_converter.c

--- main.c
+++ main.c
@@ -1,49 +1,6 @@
-#include <stdio.h>
 #define STANDARD_CHAR_OFFSET 0xffffff00
-
-/*
-    This function converts a standard text char into an ASCII char.
-    If the standard text char cannot be represented by a valid ASCII char,
-    then it will return 0 instead.
-
-    @param c the read character
-    @returns the converted ASCII value
-*/
-char convert_char(char c) {
-    char c_offset = c - STANDARD_CHAR_OFFSET;
-
-    char letter_convert = c_offset - 63;
-    int is_letter = (letter_convert >= 65 && letter_convert <= 90) || (letter_convert >= 97 && letter_convert <= 122);
-
-    // return as a letter
-    if (is_letter) {
-        return letter_convert;
-    }
-
-    // 0x50 marks the end of a string
-    if (c == 0x50) {
-        return 0;
-    }
-
-    // anything else will be represented as space for now
-    return 32;
-}
-
-/*
-    This function converts an entire string from standard text chars
-    into ASCII chars using `convert_char()`.
-
-    @param string the string to be converted
-    @param length the length of the string
-    @returns the converted string in ASCII values
-*/
-char* convert_text(char* string, int length) { 
-    for (int i = 0; i < length; i++) {
-        string[i] = convert_char(string[i]);
-    }
-
-    return string;
-}
+#include <stdio.h>
+#include "char_converter.c"
 
 int main() {
     FILE *fp;
