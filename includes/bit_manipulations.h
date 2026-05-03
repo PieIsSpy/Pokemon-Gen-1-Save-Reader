a0f7363f4023fbe88ee2debc4ae45bd26b30e15c
PieIsSpy <karldeejayomandac@gmail.com>
2026-05-03 16:21:07 +0800

    feat: added uint24_t type, implemented swap24()

--- includes/bit_manipulations.h
+++ includes/bit_manipulations.h
@@ -1,6 +1,9 @@
 #ifndef BIT_MANIPULATIONS_H
 #define BIT_MANIPULATIONS_H
 
+typedef uint8_t uint24_t[3];
+
 uint16_t swap16(uint16_t x);
+uint24_t* swap24(uint24_t x);
 
 #endif
--- src/bit_manipulations.c
+++ src/bit_manipulations.c
@@ -5,11 +5,25 @@
     This function reverses the byte order of a 16 bit
     unsigned int.
     
-    from @AmaneKai :troll_face:
-    
     @param x the 16 bit unsigned int to be bit swapped
-    @returns the bit swapped 16 bit unsigned int
+    @returns the byte swapped 16 bit unsigned int
 */
 uint16_t swap16(uint16_t x) {
     return (x >> 8) | (x << 8);
+}
+
+/*
+    This function reverses the byte order of a 24 bit unsigned int.
+
+    It functionally acts as (x << 16) | (x & 0x00FF00) | (x >> 16).
+
+    @param x the 24 bit unsigned int to be bit swapped
+    @return returns the byte swapped 24 bit unsigned int
+*/
+uint24_t* swap24(uint24_t x) {
+    uint24_t new;
+    new[0] = x[2];
+    new[1] = x[1];
+    new[2] = x[1];
+    return new;
 }
