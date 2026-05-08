25be01ecaf1fff90f1b0212c47286ad6d3a145ce
PieIsSpy <karldeejayomandac@gmail.com>
2026-05-08 22:26:13 +0800

    feat: reformat_box_pokemon() now frees the malloc of swap24()

--- src/box_pokemon.c
+++ src/box_pokemon.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "../includes/bit_manipulations.h"
 #include "../includes/box_pokemon.h"
 
@@ -11,15 +13,19 @@
     @return the reformatted `Pokemon`
 */
 BoxPokemon reformat_box_pokemon(BoxPokemon pkmn) {
+    uint24_t exp = swap24(pkmn.exp);
+
     pkmn.curHP = swap16(pkmn.curHP);
     pkmn.tid = swap16(pkmn.tid);
-    memcpy(pkmn.exp, swap24(pkmn.exp), sizeof(uint24_t));
+    memcpy(pkmn.exp, exp, sizeof(uint24_t));
     pkmn.hpEV = swap16(pkmn.hpEV);
     pkmn.atkEV = swap16(pkmn.atkEV);
     pkmn.defEV = swap16(pkmn.defEV);
     pkmn.spdEV = swap16(pkmn.spdEV);
     pkmn.speEV = swap16(pkmn.speEV);
     pkmn.ivs = swap16(pkmn.ivs);
+
+    free(exp);
 
     return pkmn;
 }
