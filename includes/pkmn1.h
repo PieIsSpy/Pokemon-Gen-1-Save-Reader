097afe0f95a28d19a5aad081bda4c4badf8e387b
PieIsSpy <karldeejayomandac@gmail.com>
2026-05-16 19:58:47 +0800

    feat: removed space padding for PKMN1 struct

--- includes/pkmn1.h
+++ includes/pkmn1.h
@@ -4,11 +4,13 @@
 #include <stdint.h>
 #include "../includes/box_pokemon.h"
 
+#pragma pack(1)
 typedef struct {
     BoxPokemon pokemon;
     uint8_t nickname[11];
     uint8_t ot_name[11];
 } PKMN1;
+#pragma pack()
 
 void save_pkmn1(PKMN1 pkmn);
 PKMN1 create_pkmn1(BoxPokemon pkmn, uint8_t* nickname, uint8_t* ot_name);
